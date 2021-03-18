23aa23de03f9f65a9a68bfb4bac00cbc2145d840
Thanh Xuan <you@example.com>
2021-03-18 15:00:11 +0700

    first commit

---
+++ app/src/main/java/com/example/aboutthanh/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.aboutthanh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btnSub).setOnClickListener {
+            addNickname(it)
+        }
+    }
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.ipName)
+        val nicknameTextView = findViewById<TextView>(R.id.txtNickname)
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        findViewById<Button>(R.id.btnSub).visibility = View.GONE
+    }
+
+
+
+}
